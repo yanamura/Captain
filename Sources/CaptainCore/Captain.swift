0d2d826cfe536ce969b6ebb47ccd091e798b84d6
Yasuharu Yanamura <yanamura3@gmail.com>
2018-04-02 18:51:38 +0900

    fix: tests

--- Sources/CaptainCore/Captain.swift
+++ Sources/CaptainCore/Captain.swift
@@ -3,8 +3,14 @@ import PathKit
 import Files
 
 public final class Captain {
+    private enum CommandType {
+        case install
+        case uninstall
+    }
+
     private let arguments: [String]
     private let path: Path
+    private let commandType: CommandType
 
     var configPath: Path {
         return path + "Captain.config.json"
@@ -15,44 +21,47 @@ public final class Captain {
     }
 
     struct Config: Codable {
-        var precommit: String
+        var precommit: String // TODO: suport array
     }
 
     public init(arguments: [String] = CommandLine.arguments, rootDir: String = Path.current.string) {
         self.arguments = arguments
+        self.path = Path(rootDir)
 
         // TODO: move to other method
         if let firstArgument = arguments.dropFirst().first, firstArgument == "install" {
-            // install Captain.config.jsonと.gitはカレントディレクトリにあるとする
+            commandType = .install
+        } else {
+            commandType = .uninstall
+        }
+    }
 
-            // configファイルを読む
-            self.path = Path(rootDir)
-            print(rootDir)
-
+    public func run() throws {
+        // install Captain.config.jsonと.gitはカレントディレクトリにあるとする
+        switch commandType {
+        case .install:
             if configPath.exists {
                 // FIXME: try!
+                // configファイルを読む
                 let data = try! configPath.read()
                 let config = try! JSONDecoder().decode(Config.self, from: data)
-                print(config)
 
                 // hookを設定する
                 if hookDirPath.exists {
                     let folder = try! Folder(path: hookDirPath.string)
                     let precommitHookFile = try! folder.createFileIfNeeded(withName: "precommit")
                     // ファイルに書き込む
-                    try! precommitHookFile.write(string: """
+                    try! precommitHookFile.append(string: """
                         ## Captain start
                         \(config.precommit)
                         ## Captain end
-                    """)
+                        """)
                 }
+            } else {
+                assert(false)
             }
-        } else {
-            self.path = Path()
+        case .uninstall:
+            break
         }
-    }
-
-    public func run() throws {
-        print("Hello")
     }
 }
--- Tests/CaptainTests/CaptainTests.swift
+++ Tests/CaptainTests/CaptainTests.swift
@@ -5,36 +5,41 @@ import Tempry
 import Files
 
 class CaptainTests: XCTestCase {
+    var currentDir: Folder!
+    var configFile: File!
+    var hooksFolder: Folder!
+
     override func setUp() {
+        // create temp directory
+        let pathString = try! Tempry.directory()
+        currentDir = try! Folder(path: pathString)
+
+        // create Captain.config.json
+        configFile = try! currentDir.createFile(named: "Captain.config.json")
+
+        // create .git/hooks dir
+        let gitFolder = try! currentDir.createSubfolder(named: ".git")
+        hooksFolder = try! gitFolder.createSubfolder(named: "hooks")
     }
 
     override func tearDown() {
+        try! currentDir.delete()
     }
 
     func test() {
-        // SETUP
-        let pathString = try! Tempry.directory()
-        // create Captain.config.json
-        let folder = try! Folder(path: pathString)
-        let configFile = try! folder.createFile(named: "Captain.config.json")
         try! configFile.write(string: "{\"precommit\": \"echo Hello\"}")
 
-        // create .git/hooks dir
-        let gitFolder = try! folder.createSubfolder(named: ".git")
-        let hooksFolder = try! gitFolder.createSubfolder(named: "hooks")
+        let captain = Captain(arguments: ["", "install"], rootDir: currentDir.path)
+        try! captain.run()
 
-        // EXECUTE
-        let captain = Captain(arguments: ["", "install"], rootDir: pathString)
-
-        // VERIFY
         XCTAssertTrue(hooksFolder.containsFile(named: "precommit"))
-
         let hookFile = try! hooksFolder.file(named: "precommit")
         let hookFileString = try! hookFile.readAsString()
-        XCTAssertEqual(hookFileString, """
-            ## Captain start
-            echo Hello
-            ## Captain end
-        """)
+        let expectString = """
+        ## Captain start
+        echo Hello
+        ## Captain end
+        """
+        XCTAssertTrue(hookFileString.hasSuffix(expectString))
     }
 }
