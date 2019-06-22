230c665e3effedd674302a11f473707cc8e27775
Yasuharu Yanamura <yanamura3@gmail.com>
2019-06-22 16:32:57 +0900

    update: PathKit 1.0.0

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "Captain",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.2"),
+        .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
         .package(url: "https://github.com/yanamura3/Tempry.git", from: "0.1.1"),
         .package(url: "https://github.com/johnsundell/files.git", from: "2.2.1"),
     ],
